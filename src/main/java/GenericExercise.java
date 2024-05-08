09f93c343052166bdb24c631a500e88c3d398fcb
Laiza Velez <lminelly6@gmail.com>
2024-05-08 14:13:37 +0000

    Done

--- src/main/java/GenericExercise.java
+++ src/main/java/GenericExercise.java
@@ -18,13 +18,14 @@ public class GenericExercise<T> {
      * Set the variable 'data' to the variable 'in'.
      * @param in an Object that will be of type T at runtime.
      */
-    public void loadGenericItem(T in){
 
+    public void loadGenericItem(T in){
+       this.data=in;
     }
     /**
      * @return T data. The type of Data will again be set when the GenericExercise object is created (at runtime).
      */
     public T returnGenericItem(){
-        return null;
+        return this.data;
     }
 }
